e5783878751284806390f622a9e382077f7bb90e
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2022-02-23 15:00:19 +0100

    TASK-53841 Deploy new contracts in Rinkeby with Strategies on Deed contract

--- src/main/webapp/vue-app/main.js
+++ src/main/webapp/vue-app/main.js
@@ -253,14 +253,15 @@ const store = new Vuex.Store({
 
               // Rinkeby
               state.etherscanBaseLink = 'https://rinkeby.etherscan.io/';
-              state.sushiswapRouterAddress = '0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506';
-              state.sushiswapPairAddress = '0x891de584b8f5ae31ac24a69dd59016646dd6db45';
               state.wethAddress = '0xc778417e063141139fce010982780140aa0cd5ab';
-              state.meedAddress = '0xD1E45d26976881F0B8f523c22D35Ba62bbcA8836';
-              state.univ2PairAddress = '0xd6b2a483d74578a95a2f7095fb5955a15c87904b';
-              state.nftAddress = '0x687689C315E96dd6b5F13b5F2B7BA2aA6b66255A';
-              state.xMeedAddress = '0x99e300801b43169B5dC19c21E83830A1c1B2F02F';
-              state.tokenFactoryAddress = '0x7c3Ab1e1b6af2e97470229186280E15a8e9463f3';
+              state.meedAddress = '0xe25aD27222D84662D7484363B4c25da123A1cB20';
+              state.tokenFactoryAddress = '0xab87e14c13C37039f14e754beFDB77f679E2C8C0';
+              state.nftAddress = '0x56B4fDF68BE8BAc27DA774e59484CD1c955c4985';
+              state.xMeedAddress = '0xea5b691e3711e3804B9DE627d7d27A1545DfA084';
+              state.tenantProvisioningAddress = '0x891a15e1378479506B8A8A4780334240034E97ea';
+              state.sushiswapRouterAddress = '0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506';
+              state.sushiswapPairAddress = '0xccbaed81a22663c18602ec23f790d4ecee843b46';
+              state.univ2PairAddress = '0x24c6839a9db67c28ae9f493e4034d6ce82c571d6';
 
               state.addSushiswapLiquidityLink = `https://app.sushi.com/add/ETH/${state.meedAddress}`;
               state.addUniswapLiquidityLink = `https://app.uniswap.org/#/add/v2/ETH/${state.meedAddress}`;
