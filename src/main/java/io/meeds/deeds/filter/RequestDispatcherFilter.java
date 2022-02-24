f87da97a583239d8d95f97f2a414e2d6a5e714f2
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2022-02-24 07:01:26 +0100

    TASK-53841 Fix Character encoding for URL / Prior to this change, the Retrieved content type encoding for URL / was ISO-8859-1 which is not convenient especially for FR language. This change will switch Servlet to a Filter to handle / request as well for Content Type Header modification.

---
+++ src/main/java/io/meeds/deeds/filter/RequestDispatcherFilter.java
@@ -0,0 +1,29 @@
+package io.meeds.deeds.filter;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class RequestDispatcherFilter implements Filter {
+
+  private static final List<String> PATHS = Arrays.asList("/", "/snapshot", "/stake", "/deeds", "/farm");
+
+  @Override
+  public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+    HttpServletRequest request = (HttpServletRequest) req;
+    HttpServletResponse response = (HttpServletResponse) res;
+    if (PATHS.contains(request.getServletPath())) {
+      response.setContentType("text/html; charset=UTF-8");
+      response.setCharacterEncoding("UTF-8");
+      RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/view.jsp");
+      dispatcher.include(request, response);// NOSONAR
+    } else {
+      chain.doFilter(request, response);
+    }
+  }
+
+}
