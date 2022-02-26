2484f4ab568d5109f42ee87869d57d2ae91e0907
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2022-02-26 08:06:30 +0100

    TASK-53841 Add HTTP Caching Header for Exchange rates Endpoint And force reload Static resources when build changes

--- deeds-dapp-service/src/main/java/io/meeds/deeds/web/utils/EnvironmentService.java
+++ deeds-dapp-service/src/main/java/io/meeds/deeds/web/utils/EnvironmentService.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of the Meeds project (https://meeds.io/).
  * 
- * Copyright (C) 2020 - 2021 Meeds Association contact@meeds.io
+ * Copyright (C) 2020 - 2022 Meeds Association contact@meeds.io
  * 
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -16,8 +16,24 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-export function getMeedsExchange(from, currency) {
-  return fetch(`/${window.parentAppLocation}/api/exchange/${(currency || 'USD').toUpperCase()}?from=${from}`, {
-    method: 'GET',
-  }).then(resp => resp && resp.ok && resp.json());
+package io.meeds.deeds.web.utils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EnvironmentService {
+
+  private static Environment environment; // NOSONAR
+
+  @Autowired
+  public EnvironmentService(Environment environment) { // NOSONAR
+    EnvironmentService.environment = environment; // NOSONAR
+  }
+
+  public static Environment getEnvironment() {
+    return environment;
+  }
+
 }
