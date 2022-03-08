3a44ded680896a4b2d71be76e294111ce3408201
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2022-03-08 15:42:07 +0100

    TASK-53841 Fix Elasticsearch indices creation without replicas by default

--- deeds-dapp-service/src/main/java/io/meeds/deeds/model/CurrencyExchangeRate.java
+++ deeds-dapp-service/src/main/java/io/meeds/deeds/model/CurrencyExchangeRate.java
@@ -1,20 +1,17 @@
 /*
  * This file is part of the Meeds project (https://meeds.io/).
- * 
  * Copyright (C) 2020 - 2022 Meeds Association contact@meeds.io
- * 
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3 of the License, or (at your option) any later version.
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
- * 
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 package io.meeds.deeds.model;
 
@@ -23,6 +20,7 @@ import java.time.LocalDate;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.*;
+import org.springframework.data.elasticsearch.annotations.Setting.SortOrder;
 
 import io.meeds.deeds.constant.Currency;
 import lombok.*;
@@ -30,7 +28,13 @@ import lombok.*;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Document(indexName = "currency_exchange_rate", createIndex = true, dynamic = Dynamic.TRUE)
+@Document(indexName = "currency_exchange_rate", createIndex = true)
+@Setting(
+    sortFields = "date",
+    sortOrders = SortOrder.desc,
+    replicas = 0,
+    shards = 1
+)
 public class CurrencyExchangeRate {
 
   @Id
