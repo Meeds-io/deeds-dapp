8a20cb36ec6ddd61df01d65a10e682eebfcf5ab3
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2022-10-11 11:42:09 +0100

    feat: Add Introduction section for Stake Meeds Page - MEED-745 (#118)

---
+++ deeds-dapp-webapp/src/main/webapp/vue-app/stake/components/StakeIntroduction.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-card flat>
+    <div class="d-flex flex-row">
+      <v-card-title class="ps-0 py-0">{{ $t('becomeHolder') }}</v-card-title>
+      <v-divider class="my-auto" />
+    </div>
+    <v-card-text class="ps-0">
+      {{ $t('becomeHolderDescription') }}
+    </v-card-text>
+  </v-card>
+</template>
