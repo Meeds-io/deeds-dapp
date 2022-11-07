a73e9facd7df4a6a8007d0d6e53e849d47d5c710
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2022-11-07 10:30:15 +0100

    feat: Add Marketplace Page Structure with static content - MEED-922 -MEED-968 (#152)

---
+++ deeds-dapp-webapp/src/main/webapp/vue-app/dapp/marketplace/components/DeedsMarketplaceIntroductionItem.vue
@@ -0,0 +1,54 @@
+<!--
+ This file is part of the Meeds project (https://meeds.io/).
+ 
+ Copyright (C) 2020 - 2022 Meeds Association contact@meeds.io
+ 
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 3 of the License, or (at your option) any later version.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public License
+ along with this program; if not, write to the Free Software Foundation,
+ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+-->
+<template>
+  <v-card
+    max-width="165px"
+    class="d-flex flex-column align-center text-center mx-sm-12 mx-auto"
+    flat>
+    <v-img
+      :src="`/${parentLocation}/static/images/${imageSrc}`"
+      max-height="65"
+      width="65"
+      contain
+      eager />
+    <strong class="tertiary--text mt-2 mb-1">{{ $t(title) }}</strong>
+    <v-card-subtitle class="pa-0 hidden-xs-only">{{ $t(subtitle) }}</v-card-subtitle>
+  </v-card>
+</template>
+<script>
+export default {
+  props: {
+    imageSrc: {
+      type: String,
+      default: null,
+    },
+    title: {
+      type: String,
+      default: null,
+    },
+    subtitle: {
+      type: String,
+      default: null,
+    },
+  },
+  computed: Vuex.mapState({
+    parentLocation: state => state.parentLocation,
+  }),
+};
+</script>
