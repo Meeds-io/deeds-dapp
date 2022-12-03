1689e804e75e0514ac59efbf65cf0f4deeeecffe
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2022-12-03 11:06:49 +0100

    fix: Fix Token asset display for Mobile when label is too long

--- deeds-dapp-webapp/src/main/webapp/vue-app/dapp/common/components/TokenAssetTemplate.vue
+++ deeds-dapp-webapp/src/main/webapp/vue-app/dapp/common/components/TokenAssetTemplate.vue
@@ -19,7 +19,7 @@
 <template>
   <v-row class="px-3 px-sm-8 pt-4 ma-0 d-flex flex-grow-1">
     <v-col class="pa-0">
-      <v-list-item class="px-0">
+      <v-list-item class="px-0 height-auto">
         <v-list-item-avatar
           v-if="$slots.image"
           class="my-0"
@@ -27,7 +27,7 @@
           <slot name="image"></slot>
         </v-list-item-avatar>
         <v-list-item-content class="py-0 flex-grow-1">
-          <v-list-item-title class="token-asset-title d-flex mb-0 font-size-normal">
+          <v-list-item-title class="token-asset-title height-auto text-wrap d-flex mb-0 font-size-normal pe-2">
             <div class="my-auto me-auto">
               <slot name="col1"></slot>
             </div>
@@ -38,7 +38,7 @@
             </div>
           </v-list-item-subtitle>
         </v-list-item-content>
-        <v-list-item-content class="py-0 flex-grow-0 overflow-visible">
+        <v-list-item-content class="py-0 flex-grow-0 overflow-visible mb-auto">
           <v-list-item-title class="token-asset-title d-flex mb-0 font-size-normal overflow-visible">
             <div class="my-auto ms-auto pt-2px">
               <slot name="col3"></slot>
