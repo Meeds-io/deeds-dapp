36102d1ee8757a67fd76056c2b1a50fab6a8304e
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2022-12-06 06:11:27 +0100

    feat: Add ES Connection Retry mechanism

--- deeds-dapp-common/src/main/java/io/meeds/deeds/elasticsearch/ElasticSearchConfig.java
+++ deeds-dapp-common/src/main/java/io/meeds/deeds/elasticsearch/ElasticSearchConfig.java
@@ -19,6 +19,8 @@ import java.time.Duration;
 
 import org.apache.commons.lang3.StringUtils;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -42,20 +44,28 @@ import io.meeds.deeds.elasticsearch.model.UserProfile;
 })
 public class ElasticSearchConfig {
 
+  private static final Logger LOG = LoggerFactory.getLogger(ElasticSearchConfig.class);
+
   @Value("${meeds.elasticsearch.username:}")
-  private String  esUsername;
+  private String              esUsername;
 
   @Value("${meeds.elasticsearch.password:}")
-  private String  esPassword;
+  private String              esPassword;
 
   @Value("${meeds.elasticsearch.url:http://127.0.0.1:9200}")
-  private String  esUrl;
+  private String              esUrl;
 
-  @Value("${meeds.elasticsearch.connectTimeout:10}")
-  private int     connectionTimeout;
+  @Value("${meeds.elasticsearch.socketTimeout:20}")
+  private int                 socketTimeout;
+
+  @Value("${meeds.elasticsearch.connectTimeout:20}")
+  private int                 connectionTimeout;
+
+  @Value("${meeds.elasticsearch.connectionRetry:30}")
+  private int                 connectionRetry;
 
   @Value("${meeds.elasticsearch.autoCreateIndex:true}")
-  private boolean createDeedIndexes;
+  private boolean             createDeedIndexes;
 
   @Bean
   public RestHighLevelClient client() {
@@ -69,6 +79,7 @@ public class ElasticSearchConfig {
       connectionBuilder.withBasicAuth(esUsername, esPassword);
     }
     connectionBuilder.withConnectTimeout(Duration.ofSeconds(connectionTimeout));
+    connectionBuilder.withSocketTimeout(Duration.ofSeconds(socketTimeout));
     ClientConfiguration clientConfiguration = connectionBuilder.build();
     return RestClients.create(clientConfiguration).rest();// NOSONAR
   }
@@ -76,23 +87,47 @@ public class ElasticSearchConfig {
   @Bean
   public ElasticsearchOperations elasticsearchTemplate(RestHighLevelClient client) {
     ElasticsearchRestTemplate elasticsearchTemplate = new ElasticsearchRestTemplate(client);
-    createIndex(elasticsearchTemplate, DeedTenant.class);
-    createIndex(elasticsearchTemplate, DeedMetadata.class);
-    createIndex(elasticsearchTemplate, DeedTenantEvent.class);
-    createIndex(elasticsearchTemplate, UserProfile.class);
+    tryConnection(elasticsearchTemplate);
+    if (createDeedIndexes) {
+      createIndex(elasticsearchTemplate, DeedTenant.class);
+      createIndex(elasticsearchTemplate, DeedMetadata.class);
+      createIndex(elasticsearchTemplate, DeedTenantEvent.class);
+      createIndex(elasticsearchTemplate, UserProfile.class);
+    }
     return elasticsearchTemplate;
+  }
+
+  private void tryConnection(ElasticsearchRestTemplate elasticsearchTemplate) {
+    int i = connectionRetry;
+    while (i-- > 0) {
+      int tentative = connectionRetry - i;
+      try {
+        if (elasticsearchTemplate.cluster().health().isTimedOut()) {
+          throw new IllegalStateException("Elasticsearch Cluster Health Check TimedOut");
+        } else {
+          LOG.info("Connection established to ES after {}/{} tentatives", tentative, connectionRetry);
+          i = 0;
+        }
+      } catch (Exception e) {
+        if (i == 0) {
+          LOG.warn("Connection failure to ES. tentative {}/{}.", tentative, connectionRetry);
+        } else {
+          LOG.info("Connection failure to ES. tentative {}/{}. Error Message: {}", tentative, connectionRetry, e.getMessage());
+          try {
+            Thread.sleep(1000);
+          } catch (InterruptedException e1) {
+            Thread.currentThread().interrupt();
+          }
+        }
+      }
+    }
   }
 
   private void createIndex(ElasticsearchRestTemplate elasticsearchTemplate, Class<?> esDocumentModelClass) {
     IndexOperations indexOperations = elasticsearchTemplate.indexOps(esDocumentModelClass);
     boolean indexExists = indexOperations.exists();
     if (!indexExists) {
-      if (createDeedIndexes) {
-        indexOperations.createWithMapping();
-      } else {
-        // Prevent Deed Tenants from creating indexes in their respective ES
-        throw new IllegalStateException("Make sure the `meeds.elasticsearch.*` configuration properties are set properly");
-      }
+      indexOperations.createWithMapping();
     }
   }
 
