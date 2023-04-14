4f7b39b2a8ff606f4813f274d2480e4f57dc9d40
MayTekayaa <100131287+MayTekayaa@users.noreply.github.com>
2023-04-14 08:47:29 +0100

    feat: Review the tenants page display -MEED-1995 - Meeds-io/MIPs#48 (#445)
    This change will fix the video's introduction emplacement for tenants page.

--- deeds-dapp-webapp/src/main/webapp/vue-app/dapp/tenants/components/DeedsTenantsIntroduction.vue
+++ deeds-dapp-webapp/src/main/webapp/vue-app/dapp/tenants/components/DeedsTenantsIntroduction.vue
@@ -47,7 +47,7 @@
     </div>
     <div v-show="!collapsed || !hasTenants">
       <div class="d-flex flex-column flex-md-row pb-6 my-16">
-        <div class="d-flex flex-column me-7">
+        <div class="d-flex flex-column my-auto me-7">
           <span class="display-1 dark-grey-color font-weight-bold">{{ $t('dapp.tenants.rentFromMarketplace.title') }}</span>
           <span class="mt-10 mb-5 mb-md-0 text-h5 dark-grey-color font-weight-light">{{ $t('dapp.tenants.rentFromMarketplace.description') }}</span>
           <div v-if="!hasTenants" class="mb-7">
@@ -81,7 +81,7 @@
           controls>
           <source :src="`${beTenantVideoLink}`" type="video/mp4">
         </video>
-        <div class="d-flex flex-column mx-0 mx-md-14">
+        <div class="d-flex flex-column my-auto mx-0 mx-md-14">
           <span class="display-1 dark-grey-color font-weight-bold">{{ $t('dapp.tenants.manageYourHub.title') }}</span>
           <span class="text-h5 dark-grey-color font-weight-light mt-10 mb-5 mb-md-0">{{ $t('dapp.tenants.manageYourHub.description') }}</span>
           <div class="mb-7">
