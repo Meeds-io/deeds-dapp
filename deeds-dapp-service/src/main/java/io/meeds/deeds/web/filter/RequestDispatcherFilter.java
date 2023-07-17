410e2ff7f82015613ecb77eebcad79c5d1bf0fcc
MayTekayaa <mtekaya@exoplatform.com>
2023-07-17 13:47:21 +0100

    MEED-2325 Add redirection to the /deeds url

--- deeds-dapp-service/src/main/java/io/meeds/deeds/web/filter/RequestDispatcherFilter.java
+++ deeds-dapp-service/src/main/java/io/meeds/deeds/web/filter/RequestDispatcherFilter.java
@@ -138,7 +138,11 @@ public class RequestDispatcherFilter extends HttpFilter {
         response.setHeader("Location", "/fr");
         response.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);
         return;
-      }
+      } else if(servletPath.equals("/deeds")) {
+        response.setHeader("Location", "/mint");
+        response.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);
+        return;
+      } 
       String eTagHeader = request.getHeader("If-None-Match");
       String eTagValue = getETagValue(request);
       if (StringUtils.equals(eTagHeader, eTagValue)) {
