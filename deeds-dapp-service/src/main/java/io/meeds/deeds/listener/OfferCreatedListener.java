f5113c65a527e1f633b942066cefe4f630f4deba
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-08-02 13:35:05 +0100

    feat: Allow disconnect a Hub - MEED-2392 - Meeds-io/MIPs#58

--- deeds-dapp-service/src/main/java/io/meeds/deeds/listener/OfferCreatedListener.java
+++ deeds-dapp-service/src/main/java/io/meeds/deeds/listener/OfferCreatedListener.java
@@ -22,7 +22,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import io.meeds.deeds.model.DeedTenantOffer;
+import io.meeds.deeds.elasticsearch.model.DeedTenantOffer;
 import io.meeds.deeds.model.UserProfileDTO;
 import io.meeds.deeds.service.OfferService;
 import io.meeds.deeds.service.UserProfileService;
--- deeds-dapp-service/src/main/java/io/meeds/deeds/storage/CurrencyExchangeRateRepository.java
+++ deeds-dapp-service/src/main/java/io/meeds/deeds/storage/CurrencyExchangeRateRepository.java
@@ -26,7 +26,7 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 import io.meeds.deeds.constant.Currency;
-import io.meeds.deeds.model.CurrencyExchangeRate;
+import io.meeds.deeds.elasticsearch.model.CurrencyExchangeRate;
 
 public interface CurrencyExchangeRateRepository extends ElasticsearchRepository<CurrencyExchangeRate, LocalDate> {
 
