40abd9609081b2b4dcc4278427c55a1e5605fa8e
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-08-16 07:06:11 +0100

    feat: Move UEM Services from Tenant Provisiong Addon to Common Module - MEED-2392 - Meeds-io/MIPs#58

---
+++ deeds-dapp-common/src/main/java/io/meeds/deeds/common/service/UEMRewardService.java
@@ -0,0 +1,87 @@
+/**
+ * This file is part of the Meeds project (https://meeds.io/).
+ *
+ * Copyright (C) 2020 - 2023 Meeds Association contact@meeds.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package io.meeds.deeds.common.service;
+
+import static io.meeds.deeds.api.constant.UEMRewardStatusType.NONE;
+import static io.meeds.deeds.common.utils.UEMRewardMapper.fromEntity;
+import static io.meeds.deeds.common.utils.UEMRewardMapper.toEntity;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Component;
+
+import io.meeds.deeds.api.constant.UEMRewardStatusType;
+import io.meeds.deeds.api.model.UEMReward;
+import io.meeds.deeds.common.elasticsearch.model.UEMRewardEntity;
+import io.meeds.deeds.common.elasticsearch.storage.UEMRewardRepository;
+import io.meeds.deeds.common.model.RewardPeriod;
+import io.meeds.deeds.common.utils.UEMRewardMapper;
+
+@Component
+public class UEMRewardService {
+
+  private static final List<UEMRewardStatusType> NOT_SENT_STATUSES = Stream.of(NONE).toList();
+
+  @Autowired
+  private UEMRewardRepository                    rewardRepository;
+
+  public Page<UEMReward> getRewards(String hubAddress, Pageable pageable) {
+    pageable = PageRequest.of(pageable.getPageNumber(),
+                              pageable.getPageSize(),
+                              pageable.getSortOr(Sort.by(Direction.DESC, "fromDate")));
+    if (StringUtils.isBlank(hubAddress)) {
+      return rewardRepository.findAll(pageable)
+                             .map(UEMRewardMapper::fromEntity);
+    } else {
+      return rewardRepository.findByHubAddress(hubAddress, pageable)
+                             .map(UEMRewardMapper::fromEntity);
+    }
+  }
+
+  public Stream<UEMReward> getPendingRewards() {
+    return rewardRepository.findByStatusIn(NOT_SENT_STATUSES)
+                           .map(UEMRewardMapper::fromEntity);
+  }
+
+  public UEMReward saveReward(UEMReward reward) {
+    UEMRewardEntity rewardEntity = toEntity(reward);
+    rewardRepository.save(rewardEntity);
+    return fromEntity(rewardEntity);
+  }
+
+  public UEMReward getReward(RewardPeriod period) {
+    return rewardRepository.findByFromDateIsAndToDateIs(period.getFrom(), period.getTo())
+                           .map(UEMRewardMapper::fromEntity)
+                           .orElse(null);
+  }
+
+  public UEMReward getRewardById(String rewardId) {
+    return rewardRepository.findById(rewardId)
+                           .map(UEMRewardMapper::fromEntity)
+                           .orElse(null);
+  }
+
+}
---
+++ deeds-dapp-service/src/main/java/io/meeds/dapp/service/HubRewardComputingService.java
@@ -0,0 +1,183 @@
+/**
+ * This file is part of the Meeds project (https://meeds.io/).
+ *
+ * Copyright (C) 2020 - 2023 Meeds Association contact@meeds.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package io.meeds.dapp.service;
+
+import static io.meeds.deeds.api.constant.HubReportStatusType.NONE;
+import static io.meeds.deeds.api.constant.HubReportStatusType.PENDING_REWARD;
+import static io.meeds.deeds.api.constant.HubReportStatusType.REWARDED;
+import static io.meeds.deeds.api.constant.HubReportStatusType.SENT;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.time.Duration;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import io.meeds.deeds.api.constant.HubReportStatusType;
+import io.meeds.deeds.api.constant.ObjectNotFoundException;
+import io.meeds.deeds.api.model.HubReport;
+import io.meeds.deeds.api.model.UEMReward;
+import io.meeds.deeds.common.constant.DeedCard;
+import io.meeds.deeds.common.model.RewardPeriod;
+import io.meeds.deeds.common.service.HubReportService;
+import io.meeds.deeds.common.service.TenantService;
+import io.meeds.deeds.common.service.UEMRewardService;
+
+@Component
+public class HubRewardComputingService {
+
+  public static final double                     UEM_REWARD_PERIOD_IN_DAYS = 7d;
+
+  private static final List<HubReportStatusType> VALID_STATUSES            = Stream.of(NONE,
+                                                                                       SENT,
+                                                                                       PENDING_REWARD,
+                                                                                       REWARDED)
+                                                                                   .toList();
+
+  @Autowired
+  private UEMRewardService                       rewardService;
+
+  @Autowired
+  private HubReportService                       reportService;
+
+  @Autowired
+  private TenantService                          tenantService;
+
+  protected HubReport computeUEMHubReward(String hash, UEMReward reward) {
+    HubReport report = reportService.getReport(hash);
+    if (StringUtils.isBlank(report.getRewardId())) {
+      report.setRewardId(reward.getId());
+    }
+
+    HubReport lastRewardedReport = computeLastPeriodUemRewardAmount(report);
+    computeLastPeriodUemRewardAmountPerPeriod(report, lastRewardedReport, reward);
+    computeHubRewardAmountPerPeriod(report, lastRewardedReport, reward);
+    computeMintingPower(report);
+
+    double ed = report.getEd();
+    double ew = reward.getEw();
+    double dr = report.getDr();
+    double ds = report.getDs();
+    double mp = report.getMp();
+
+    double uemRewardIndex = ew == 0 ? 0d
+                                    : BigDecimal.valueOf(ed)
+                                                .divide(BigDecimal.valueOf(ew), MathContext.DECIMAL128)
+                                                .multiply(BigDecimal.valueOf(dr))
+                                                .multiply(BigDecimal.valueOf(ds))
+                                                .multiply(BigDecimal.valueOf(mp))
+                                                .doubleValue();
+    report.setUemRewardIndex(uemRewardIndex);
+    return report;
+  }
+
+  private HubReport computeLastPeriodUemRewardAmount(HubReport report) {
+    HubReport lastRewardedReport = reportService.getLastReport(report.getHubAddress(),
+                                                               report.getSentDate(),
+                                                               report.getHash(),
+                                                               VALID_STATUSES);
+    report.setLastPeriodUemRewardAmount(lastRewardedReport == null ? 0d : lastRewardedReport.getUemRewardAmount());
+    return lastRewardedReport;
+  }
+
+  private void computeLastPeriodUemRewardAmountPerPeriod(HubReport report,
+                                                         HubReport lastRewardedReport,
+                                                         UEMReward currentReward) {
+    if (lastRewardedReport == null) {
+      // Number of weeks since last period UEM reward sent to Hub
+      report.setLastPeriodUemDiff(0);
+      report.setLastPeriodUemRewardAmountPerPeriod(0d);
+    } else {
+      BigDecimal diffWeeks = getDiffWeeks(currentReward, lastRewardedReport);
+      report.setLastPeriodUemDiff(diffWeeks.doubleValue());
+
+      double lastPeriodUemRewardAmountPerPeriod = report.getLastPeriodUemDiff() == 0d ? 0d
+                                                                                      : BigDecimal.valueOf(lastRewardedReport.getUemRewardAmount())
+                                                                                                  .divide(diffWeeks,
+                                                                                                          MathContext.DECIMAL128)
+                                                                                                  .doubleValue();
+      report.setLastPeriodUemRewardAmountPerPeriod(lastPeriodUemRewardAmountPerPeriod);
+    }
+  }
+
+  private void computeHubRewardAmountPerPeriod(HubReport report,
+                                               HubReport lastRewardedReport,
+                                               UEMReward currentReward) {
+    if (lastRewardedReport == null) {
+      // Number of weeks since last period, 1 if no hub rewards
+      report.setHubRewardLastPeriodDiff(1d);
+      report.setHubRewardAmountPerPeriod(getHubRewardAmountPerPeriod(report));
+    } else {
+      BigDecimal diffWeeks = getDiffWeeks(currentReward, lastRewardedReport);
+      report.setHubRewardLastPeriodDiff(diffWeeks.doubleValue());
+
+      double hubRewardAmountPerPeriod = report.getHubRewardLastPeriodDiff() == 0d ? getHubRewardAmountPerPeriod(report)
+                                                                                  : BigDecimal.valueOf(report.getHubRewardAmount())
+                                                                                              .divide(diffWeeks,
+                                                                                                      MathContext.DECIMAL128)
+                                                                                              .doubleValue();
+      report.setHubRewardAmountPerPeriod(hubRewardAmountPerPeriod);
+    }
+  }
+
+  private double getHubRewardAmountPerPeriod(HubReport report) {
+    long diffDays = Duration.between(report.getFromDate(),
+                                     report.getToDate())
+                            .toDays();
+    BigDecimal diffWeeks = BigDecimal.valueOf(diffDays)
+                                     .divide(BigDecimal.valueOf(UEM_REWARD_PERIOD_IN_DAYS), MathContext.DECIMAL128);
+    return diffDays == 0 ? 0d
+                         : BigDecimal.valueOf(report.getHubRewardAmount())
+                                     .divide(diffWeeks, MathContext.DECIMAL128)
+                                     .doubleValue();
+  }
+
+  private BigDecimal getDiffWeeks(UEMReward currentReward, HubReport lastRewardedReport) {
+    long diffDays = getDiffDays(currentReward, lastRewardedReport);
+    return diffDays == 0 ? BigDecimal.ZERO
+                         : BigDecimal.valueOf(diffDays)
+                                     .divide(BigDecimal.valueOf(UEM_REWARD_PERIOD_IN_DAYS), MathContext.DECIMAL128);
+  }
+
+  private long getDiffDays(UEMReward currentReward, HubReport lastRewardedReport) {
+    UEMReward lastUemRewarded = rewardService.getReward(RewardPeriod.getPeriod(lastRewardedReport.getSentDate()));
+    if (lastUemRewarded == null) {
+      return 0;
+    }
+    return Duration.between(lastUemRewarded.getFromDate(),
+                            currentReward.getFromDate())
+                   .toDays();
+  }
+
+  private void computeMintingPower(HubReport report) {
+    try {
+      short cardType = tenantService.getCardType(report.getDeedId());
+      DeedCard card = DeedCard.values()[cardType];
+      report.setMp(card.getMintingPower());
+    } catch (ObjectNotFoundException e) {
+      throw new IllegalStateException("Error retrieving deed card type with id '#"
+          + report.getDeedId()
+          + "' from blockchain", e);
+    }
+  }
+
+}
