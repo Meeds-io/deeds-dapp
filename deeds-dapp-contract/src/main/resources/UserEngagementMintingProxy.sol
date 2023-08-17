5a169cd59a9cd2e9c9f41ee31153212ca02df584
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-08-17 17:18:30 +0100

    feat: Implement Rewarding Contract - MEED-2392 - Meeds-io/MIPs#58

---
+++ deeds-dapp-contract/src/main/resources/UserEngagementMintingProxy.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.9;
+
+import "./abstract/ERC1967Proxy.sol";
+
+contract UserEngagementMintingProxy is ERC1967Proxy {
+
+    constructor(address _logic, bytes memory _data) payable ERC1967Proxy(_logic, _data) {
+    }
+
+}
