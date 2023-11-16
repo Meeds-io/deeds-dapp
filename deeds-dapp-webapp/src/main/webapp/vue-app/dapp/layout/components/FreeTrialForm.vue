32642294b952fefc389ceb314b19ed5bb53ed93b
MayTekayaa <100131287+MayTekayaa@users.noreply.github.com>
2023-11-16 16:03:08 +0100

    feat: Add the email confirmation step for the trial request - MEED-2829 - meeds-io/MIP#102 (#672)
    This change will add the email confirmation step for the trial request.

--- deeds-dapp-webapp/src/main/webapp/vue-app/dapp/layout/components/FreeTrialForm.vue
+++ deeds-dapp-webapp/src/main/webapp/vue-app/dapp/layout/components/FreeTrialForm.vue
@@ -175,10 +175,6 @@ export default {
   },
   computed: Vuex.mapState({
     tourURL: state => state.tourURL,
-    validEmail() {
-      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      return emailRegex.test(this.email);
-    },
     disabledFormButton() {
       return !this.fullname || !this.position || !this.organization || !this.motivation || !this.validEmail || this.motivation.length > 250;
     },
