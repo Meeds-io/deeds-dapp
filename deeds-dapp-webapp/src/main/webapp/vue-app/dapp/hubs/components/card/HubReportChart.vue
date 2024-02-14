cd5e032c01aaba380792649fbbf473d232e9b9ef
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2024-02-14 11:48:18 +0100

    fix: Fix Hub Chart Card border radius and increase title padding - MEED-3347 - Meeds-io/MIPs#106 (#731)

--- deeds-dapp-webapp/src/main/webapp/vue-app/dapp/hubs/components/card/HubReportChart.vue
+++ deeds-dapp-webapp/src/main/webapp/vue-app/dapp/hubs/components/card/HubReportChart.vue
@@ -1,34 +1,38 @@
 <!--
+
  This file is part of the Meeds project (https://meeds.io/).
- 
+
  Copyright (C) 2020 - 2024 Meeds Association contact@meeds.io
- 
+
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU Lesser General Public
  License as published by the Free Software Foundation; either
  version 3 of the License, or (at your option) any later version.
+
  This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  Lesser General Public License for more details.
- 
+
  You should have received a copy of the GNU Lesser General Public License
  along with this program; if not, write to the Free Software Foundation,
  Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 -->
 <template>
   <v-card
     v-if="reports.length > 1"
-    class="d-flex flex-column reportChartParent"
+    class="d-flex flex-column rounded-lg reportChartParent"
     height="270"
     max-height="270"
     outlined>
-    <div class="absolute-horizontal-center z-index-two mt-2">
+    <div class="absolute-horizontal-center z-index-two mt-4">
       {{ $t('uem.hubEngagementScore') }}
     </div>
     <v-card
       id="reportChart"
       ref="reportChart"
+      class="rounded-lg"
       height="100%"
       width="100%"
       flat />
