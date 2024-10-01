4682c8ede430859f6666ec7d1e08b466c6c36812
Patrice Lamarque <patrice.lamarque@meedslab.com>
2024-10-01 14:36:31 +0200

    disable bottom bar on mobile
    Before this change, a nav bar was displayed on mobile and on (non-static) pages of the dApp. 
    
    This change changes this behavior, so no bottom nav is displayed on mobile.

--- deeds-dapp-webapp/src/main/webapp/vue-app/dapp/layout/components/SiteContent.vue
+++ deeds-dapp-webapp/src/main/webapp/vue-app/dapp/layout/components/SiteContent.vue
@@ -64,7 +64,7 @@ export default {
     staticPage: state => state.staticPage,
     isMobile: state => state.isMobile,
     mobileNavigationBar() {
-      return !this.staticPage && this.isMobile;
+      return false;//!this.staticPage && this.isMobile;
     },
   }),
   created() {
